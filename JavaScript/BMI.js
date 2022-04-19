285038b1fd7c4d16f8fef885b8e1bd8ea214f83b
Daxil Patel <62294074+Daxil44@users.noreply.github.com>
2022-04-18 22:14:16 -0400

    Delete JavaScript directory

--- JavaScript/BMI.js
+++
@@ -1,7 +0,0 @@
-function getBMI()
-{
-        Height = document.getElementById("Height").value;
-        Weight = document.getElementById("Weight").value;
-        document.getElementById("BMI").innerHTML = Weight/((Height/100) * (Height/100));
-}
-
